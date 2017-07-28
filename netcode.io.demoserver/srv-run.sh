cb81f89da3c5a2d2a295044282e2b39f41235851
June Rhodes <jrhodes@redpointgames.com.au>
2017-07-29 06:29:03 +1000

    Make Docker image work on read-only filesystem

--- netcode.io.demoserver/srv-run.sh
+++ netcode.io.demoserver/srv-run.sh
@@ -4,10 +4,4 @@ set -e
 set -x
 
 cd /srv
-if [ -d ../lib64 ]; then
-  cp libnetcode64.so libnetcode.so
-else
-  cp libnetcode32.so libnetcode.so
-fi
-
 LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH /usr/bin/mono netcode.io.demoserver.exe $*
